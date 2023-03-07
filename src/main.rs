633452da4cd0dd96406bdb14cc4f045f79a6c2e2
RobAshby4 <robert.n.ashby@gmail.com>
2023-03-07 11:10:29 -0800

    done working for now

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,28 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
+fn get_text(buf: Vec<String>, start: i32, height: u16) -> Result<String, _> {
+    let mut retbuf = String::new();
+    for x in start..height as i32 {
+        retbuf.push_str( &buf.get(x as usize).clone());
+    }
+    return Ok(retbuf);
+}
+
 fn main() -> Result<(), io::Error> {
-    let mut buf = String::new();
+    if atty::is(atty::Stream::Stdin) {
+        println!("No input on stdin, closing");
+        return Ok(());
+    }
+
+    let mut buf: Vec<String>= Vec::new();
     loop {
-        match io::stdin().read_line(&mut buf) {
-            Ok(siz) => if siz == 0 {break},
+        let mut linebuf = String::new();
+        match io::stdin().read_line(&mut linebuf) {
+            Ok(siz) => {
+                if siz == 0 {break};
+                buf.push(linebuf);
+            },
             Err(_) => break,
         };
     }
@@ -32,12 +49,12 @@ fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     loop {
-        let tempbuf = buf.clone();
         terminal.draw(|x| {
             let size = x.size();
             let block = Block::default()
                 .title("output")
                 .borders(Borders::ALL);
+            let tempbuf = get_text(buf.clone(), 0, size.height);
             let para = Paragraph::new(tempbuf).block(block);
             x.render_widget(para, size);
         })?;
